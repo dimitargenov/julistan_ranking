cd4e27ab1b4d5f51eae265b56e2435d4ba263b87
Dimitar Genov <mitko@devision.bg>
2018-11-10 18:33:02 +0200

    genera ranking added

---
+++ julbem_general.py
@@ -0,0 +1,56 @@
+from StravaRequest import StravaRequest
+import StravaConfig
+from GoogleSpreadSheet import GoogleSpreadSheet
+import numpy as np
+import datetime
+import math
+
+now = datetime.datetime.now()
+
+JULBEM_SPREADSHEET_ID = '1UsFnri7Dfcl0ETlHfVSOMrsCIGoCQOnLIQHC__GMwWs'
+TICHANE_COEF = 6
+
+def sortByPoints(values):
+    return sorted(values, key=lambda a_entry: a_entry[1], reverse = True) 
+
+def displayNumber(value):
+    return 1
+
+def addToGeneral(weekRanking, athletes):
+    for row in weekRanking:
+        if (len(row) < 3) or (row[2] == "D+[m]"):
+            continue
+        if row[0] not in athletes:
+            athletes[row[0]] = list()
+            athletes[row[0]].append(0)
+            athletes[row[0]].append(1)
+            athletes[row[0]].append(2)
+        athletes[row[0]][0] = row[0]
+        athletes[row[0]][1] += float(row[5])*1.0
+        athletes[row[0]][2] += float(row[1])*1.0
+
+    return athletes
+
+def main():
+    ## Read from spreadsheet
+    athletes = {}
+    spreadsheet = GoogleSpreadSheet()
+    weekSheets = ['W1', 'W2']
+
+    for sheet in weekSheets:
+        rangeName = sheet + "!C3:M"
+        wRanking = spreadsheet.read(rangeName, JULBEM_SPREADSHEET_ID)
+        athletes = addToGeneral(wRanking, athletes)
+
+    tableHeaderRow = ['Athlete', 'Total', 'Distance[km]']
+
+    sortedResults = sortByPoints(athletes.values())
+    sortedResults.insert(0, tableHeaderRow)
+    sortedResults.append(['Last update', now.strftime("%Y-%m-%d %H:%M")])
+
+    ## Write to spreadsheet
+    rangeName = "General!C2:M"
+    spreadsheet.write(rangeName, sortedResults, JULBEM_SPREADSHEET_ID)   
+
+if __name__ == '__main__':
+    main()
--- julistan_obem.py
+++ julistan_obem.py
@@ -1,9 +1,13 @@
 from StravaRequest import StravaRequest
 import StravaConfig
 from GoogleSpreadSheet import GoogleSpreadSheet
+import numpy as np
+import datetime
+import math
+
+now = datetime.datetime.now()
 
 JULBEM_SPREADSHEET_ID = '1UsFnri7Dfcl0ETlHfVSOMrsCIGoCQOnLIQHC__GMwWs'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 TICHANE_COEF = 6
 
 def calculatePace(time, distance):
@@ -16,35 +20,51 @@ def calculateCoefJulqga(paceMin, paceSec):
 def calculatePoints(distance, elevation, kj):
     return round((distance + elevation*0.01)*kj, 1)
 
+def sortByPoints(values):
+    return sorted(values, key=lambda a_entry: a_entry[5], reverse = True) 
+
+def displayNumber(value):
+    return 1
+
+def fillInAthleteInfo(row):
+    paceMin = 0
+    paceSec = 0
+    [paceMin, paceSec] = calculatePace(row['moving_time'],row['distance'])
+    distance = float(round(row['distance']/1000,1))
+    elevation = int(math.ceil(row['elev_gain']))
+    name = row['athlete_firstname'] + ' ' + row['athlete_lastname']
+    pace = str(paceMin) + ':' + str(paceSec).zfill(2)
+    kj = calculateCoefJulqga(paceMin, paceSec)
+    points = calculatePoints(distance, elevation, calculateCoefJulqga(paceMin, paceSec))
+
+    athlete = list()
+    athlete.append(name)
+    athlete.append(str(distance).zfill(1))
+    athlete.append(elevation)
+    athlete.append(pace)
+    athlete.append(kj)
+    athlete.append(points)
+    
+    return athlete
+
 def main():
     strava = StravaRequest(StravaConfig.url, StravaConfig.headers)
     leaderboard = strava.getLeaderboard()
-    values = [['Rank', 'Athlete', 'Distance', 'Elevation gain (m)', 'Pace', 'KJ', 'Points']]
+    tableHeaderRow = ['Athlete', 'Distance[km]', 'D+[m]', 'Pace[min/km]', 'KJ', 'Total']
+    values = []
+    counter = 1
     for row in leaderboard.json()['data']:
-        paceMin = 0
-        paceSec = 0
-        [paceMin, paceSec] = calculatePace(row['moving_time'],row['distance'])
-        distance = float(row['distance']/1000)
-        elevation = int(row['elev_gain'])
-        athlete = list()
-        athlete.append(row['rank'])
-        athlete.append(row['athlete_firstname'] + ' ' + row['athlete_lastname'])
-        athlete.append(round(distance,2))
-        athlete.append(elevation)
-        athlete.append(str(paceMin) + ':' + str(paceSec).zfill(2))
-        athlete.append(calculateCoefJulqga(paceMin, paceSec))
-        athlete.append(calculatePoints(distance, elevation, calculateCoefJulqga(paceMin, paceSec)))
-        #athlete.append(row['moving_time'])
-        #athlete.append(row['elapsed_time'])
-        #athlete.append(row['athlete_id'])
-        #athlete_picture_url
-        #num_activities
-        values.append(athlete)
+        values.append(fillInAthleteInfo(row))
+        counter += 1
+
+    sortedResults = sortByPoints(values)
+    sortedResults.insert(0, tableHeaderRow)
+    sortedResults.append(['Last update', now.strftime("%Y-%m-%d %H:%M")])
 
     ## Write to spreadsheet
     spreadsheet = GoogleSpreadSheet()
-    range_name = "Sheet1!B3:M"
-    spreadsheet.write(range_name, values, JULBEM_SPREADSHEET_ID)
+    rangeName = "W2!C3:M"
+    spreadsheet.write(rangeName, sortedResults, JULBEM_SPREADSHEET_ID)
 
 if __name__ == '__main__':
     main()
