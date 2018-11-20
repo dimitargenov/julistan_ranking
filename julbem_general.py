849fa72e410b8121c115a971dbdcfe89f1a87857
Dimitar Genov <mitko@devision.bg>
2018-11-20 13:19:37 +0200

    add elevation in general

--- julbem_general.py
+++ julbem_general.py
@@ -16,6 +16,18 @@ def sortByPoints(values):
 def displayNumber(value):
     return 1
 
+def addDiff(athletes):
+    leaderPoints = 0
+    counter = 0
+    for row in athletes:
+        if leaderPoints == 0:
+            leaderPoints = row[1]
+            continue
+        # print(leaderPoints)
+        # exit()
+        diff = float(leaderPoints) - float(row[1])
+        row.append(diff)
+
 def addToGeneral(weekRanking, athletes):
     for row in weekRanking:
         if (len(row) < 3) or (row[2] == "D+[m]"):
@@ -23,11 +35,13 @@ def addToGeneral(weekRanking, athletes):
         if row[0] not in athletes:
             athletes[row[0]] = list()
             athletes[row[0]].append(0)
-            athletes[row[0]].append(1)
-            athletes[row[0]].append(2)
-        athletes[row[0]][0] = row[0]
-        athletes[row[0]][1] += float(row[5])
-        athletes[row[0]][2] += float(row[1])
+            athletes[row[0]].append(1) #Total points
+            athletes[row[0]].append(0) #Distance
+            athletes[row[0]].append(0) #Elevation
+        athletes[row[0]][0] = row[0] #Name
+        athletes[row[0]][1] += float(row[5]) #Total points
+        athletes[row[0]][2] += float(row[1]) #Distance
+        athletes[row[0]][3] += int(row[2]) #Elevation
 
     return athletes
 
@@ -35,16 +49,17 @@ def main():
     ## Read from spreadsheet
     athletes = {}
     spreadsheet = GoogleSpreadSheet()
-    weekSheets = ['W1', 'W2', 'W3']
+    weekSheets = ['W1', 'W2', 'W3', 'W4']
 
     for sheet in weekSheets:
         rangeName = sheet + "!C3:M"
         wRanking = spreadsheet.read(rangeName, JULBEM_SPREADSHEET_ID)
         athletes = addToGeneral(wRanking, athletes)
 
-    tableHeaderRow = ['Athlete', 'Total', 'Distance[km]']
+    tableHeaderRow = ['Athlete', 'Total', 'Distance[km]', 'Elevation[m]', 'Diff']
 
     sortedResults = sortByPoints(athletes.values())
+    addDiff(sortedResults)
     sortedResults.insert(0, tableHeaderRow)
     sortedResults.append(['Last update', now.strftime("%Y-%m-%d %H:%M")])
 
